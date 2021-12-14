5185492e9fc8ef8b9d2181e4e4417dab801dba7b
saragussoni <gussoni.sara@gmail.com>
2021-12-14 22:22:51 +0100

    adding some comments

--- public/sketch.js
+++ public/sketch.js
@@ -1,6 +1,6 @@
 let clientSocket= io();
 
-
+//preload the images
 function preload(){
 
 bg= loadImage("assets/skyline.png")
@@ -15,49 +15,34 @@ console.log(clientSocket.id);
 }
 
 function newBroadcast(data){
-  console.log(data);
-  //noStroke();
-  //fill("pink");
-  image(beetrail, data.x, data.y,20,20)
-
-  //rect(data.x, data.y, 3)
-  //rect(data.x+3, data.y+3, 3)
+  //console.log(data);
+  //Drawing bees (smaller than the cursor) with data from the other clients
+image(beetrail, data.x, data.y,37,30)
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(bg);
-  frameRate(600)
 }
 
+//title or instructions
 function draw() {
-
   push();
   noStroke();
-
   fill("white");
   textSize(30);
   textAlign(CENTER);
   text("write your message and fill the sky with bees", width / 2, height / 9 - 40);
   pop();
-
-  //noStroke()
-  //textSize(30);
-  //textAlign(CENTER);
-  //text("write your message and fill the sky with bees", width / 2, height / 9 - 40);
-  //fill("white");
-  //rect(mouseX, mouseY, 7);
 }
 
+//when i move my cursor i leave behind a trail and send a message to the Server
 function mouseMoved(){
-
   push();
   fill(255, 255, 255, 170);
   noStroke(8);
   rect(mouseX, mouseY, 5, 5);
-  //image(beetrail, mouseX, mouseY,20,20)
   pop();
-
 
   let message = {
     x:mouseX,
